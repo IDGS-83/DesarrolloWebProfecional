3df64b1dd42aa3d38f4d8c3028b2636d607504d3
francisco jesus perez medina <franperez159264@gmail.com>
2026-01-24 09:55:39 -0600

    controladore home

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from controllers.homecontroller import BlueprintHome
 
 def create_app():
     app = Flask(__name__)
 
+    app.register_blueprint(BlueprintHome)
     @app.route('/')
     def name():
-        return {'mensaje': 'hola mundo'}, 400
+        return {'mensaje': 'hola mundo'}, 200
 
     return app
 
