7c2efdb3c5ba38a6214014d06f3e124f512fa649
francisco jesus perez medina <franperez159264@gmail.com>
2026-01-28 08:32:40 -0600

    migraciones creadas

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from controllers.homecontroller import BlueprintHome
+from extensions import db, migrate
+from config import Config
 
 def create_app():
     app = Flask(__name__)
-
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app, db)
     app.register_blueprint(BlueprintHome)
+    from models.user import User
+
     @app.route('/')
-    def name():
+    def home():
         return {'mensaje': 'hola mundo'}, 200
 
     return app
